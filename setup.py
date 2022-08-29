a2726eb9056c39ab41f46d00865d414241d0c69c
miaog001 <bobby.miao@disney.com>
2022-08-29 14:16:07 +0800

    asdf

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bblt',
-    version='1.0.6',
+    version='1.0.7',
     keywords='launchtest',
     description='APP Launch Test',
     license='MIT License',
